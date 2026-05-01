d6ccf76a9fe6542c7f8b6bceef829d512b440b5c
bl4ckimpact <148637377+bl4ckimpact@users.noreply.github.com>
2026-05-01 19:39:33 +0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 1. Tambahkan interface WAN USB ke /etc/config/network
+sed -i '$a \\nconfig interface "wan_usb"\n\toption proto "dhcp"\n\toption device "usb0"\n\toption peerdns "1"' package/base-files/files/etc/config/network
+
+# 2. Masukkan wan_usb ke Zona WAN di firewall agar NAT aktif
+# Mencari baris 'list network wan' dan menambahkan 'list network wan_usb' di bawahnya
+sed -i '/list network .wan./a \	list network "wan_usb"' package/network/config/firewall/files/firewall.config
+
+# 3. Opsional: Mengubah IP LAN agar tidak bentrok dengan IP Tethering HP (biasanya 192.168.42.x)
+# Kita set IP Router ke 192.168.10.1
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
